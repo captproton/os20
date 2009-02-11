2d896a30f1a465b1f28d63ebd746b23c8f9af41d
Heather Tanner <heathertanner@heather-tanners-macbook.local>
2009-02-11 15:16:56 -0800

    removing migration based on Discussion

--- db/migrate/20090124043507_commentator_to_user.rb
+++
@@ -1,9 +0,0 @@
-class CommentatorToUser < ActiveRecord::Migration
-  def self.up
-    rename_column :discussions, :commentator_id, :user_id
-  end
-
-  def self.down
-    rename_column :discussions, :user_id, :commentator_id
-  end
-end
